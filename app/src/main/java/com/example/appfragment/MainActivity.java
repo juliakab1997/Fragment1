24895d32c4545cce2eaf0b561de3d041b6895cc2
julie <juliamkb97@gmail.com>
2021-01-04 23:35:31 +0100

    fragment1

---
+++ app/src/main/java/com/example/appfragment/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.appfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void showText(String topImageText, String bottomImageText) {
+        BottomFragment bottomFragment
+                = (BottomFragment) this.getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_bottom);
+        bottomFragment.showText(topImageText, bottomImageText);
+    }
+}
